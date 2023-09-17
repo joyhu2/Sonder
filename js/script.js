211a1f0499f682d22dcf27f9d9f0974707c15746
Joy <jing.hu2@uqconnect.edu.au>
2023-09-17 20:30:30 +1000

    Edit audio control

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
 document.addEventListener("readystatechange", function(event) {
-	if(event.target.readyState == "interactive") {
+  if(event.target.readyState == "interactive") {
 
-		// Adding a "JavaScript is Enabled" Body Class
+      // Adding a "JavaScript is Enabled" Body Class
+      document.querySelector("body").classList.add("js");
 
-		document.querySelector("body").classList.add("js");
-		// Or this shorter version: document.body.classList.add("js");
+      // Podcast Player Controls
+      document.querySelector("#podcast .player-controls a:first-child").addEventListener("click", function(event) {
+          this.parentElement.classList.toggle("playing");
+          const audioElement = document.getElementById("audio");
+          audioElement.play();
+          event.preventDefault();
+      });
 
-		// Podcast Player Controls
-
-		document.querySelector("#podcast .player-controls a:first-child").addEventListener("click", function(event) {
-			this.parentElement.classList.toggle("playing");
-			document.querySelector("#podcast audio").play();
-			event.preventDefault();
-		});
-
-		document.querySelector("#podcast .player-controls a:last-child").addEventListener("click", function(event) {
-			this.parentElement.classList.toggle("playing");
-			document.querySelector("#podcast audio").pause();
-			event.preventDefault();
-		});
-		
-	
-	}
+      document.querySelector("#podcast .player-controls a:last-child").addEventListener("click", function(event) {
+          this.parentElement.classList.toggle("playing");
+          const audioElement = document.getElementById("audio");
+          audioElement.pause();
+          event.preventDefault();
+      });
+      
+  }
 });
+
 
 const baseUrl = 'https://data.gov.au/data/api/3/action/datastore_search';
 
@@ -70,21 +69,3 @@ getNextSong();
 document.getElementById("audio").addEventListener("ended", () => {
   getNextSong(); // Fetch and play the next song
 });
-
-
-/*fetch(urlWithParams, requestOptions)
-    .then(response => response.json())
-    .then(data => {
-		const records = data.result.records[0];  // Access the first record in the records array
-        const bird_song_url_full = records.URL;
-        const bird_song_url = bird_song_url_full.split(";")[0] + ".mp3";
-
-		const audioElement = document.getElementById("audio");
-        const sourceElement = audioElement.querySelector("source");
-
-		// Update the src attribute
-        sourceElement.setAttribute("src", bird_song_url);
-		audioElement.load();
-    })
-    .catch(error => console.log('error', error));
-*/
